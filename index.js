0142eee6d87f8eeda7fb3bcc50f41a7e448ca3e4
CKK <christopherki-sopkim@Christophers-MacBook-Pro.local>
2018-05-15 12:31:42 -0400

    New

---
+++ index.js
@@ -0,0 +1,20 @@
+function setViewPort() {
+  if($(window).width() < 300) {
+        x=1;
+        if(x==1)
+      {
+          $('meta[name=viewport]').remove();
+          $('head').append('<meta name="viewport" content="width=300px, initial-scale=1">');
+          x=0;
+      };
+  };
+}
+
+function openNav(x) {
+  document.getElementById("nav-menu").style.width = "110px";
+  document.getElementById("nav-menu").style.height = "100%";
+}
+function closeNav(x) {
+  document.getElementById("nav-menu").style.width = "0";
+  document.getElementById("nav-menu").style.height = "0%";
+}
